949635c806382001ecb172626d2e8efa127ee930
bhobo <bhobotosh345@gmail.com>
2026-06-08 13:20:09 +0600

    change code

---
+++ lib/src/models/repository/repository.dart
@@ -0,0 +1,16 @@
+import 'dart:core';
+
+class Repository{
+  double add (double a, double b){
+    return a + b;
+  }
+  double subtract (double a, double b){
+    return a - b;
+  }
+  double multiply(double a, double b){
+    return a  *  b;
+  }
+  double divide (double a , double b){
+    return a / b;
+  }
+}
--- lib/src/viewmodel/calculator_model.dart
+++ lib/src/viewmodel/calculator_model.dart
@@ -1,34 +1,41 @@
-class CalculatorModel extends ChangeNotifier {
-  CalculatorModel(required this._repository);
-  final Repository repositroy;
+import 'package:flutter/cupertino.dart';
+
+import '../models/repository/repository.dart';
+
+
+class CalculatorViewModel extends ChangeNotifier {
+  CalculatorViewModel(this._repository);
+
+  final Repository _repository;
+
   double result = 0;
 
   void add(double a , double b) {
-    _repositroy.add(a,b);
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
+  result = _repository.add(a,b);
+  // This call tells the widgets that are listening to this model to rebuild.
+  notifyListeners();
   }
 
   void subtract(double a, double b){
-    result = repository.subtract(a,b);
-    notifyListeners();
+  result = _repository.subtract(a,b);
+  notifyListeners();
 
   }
 
   void multiply (double a, double b){
-    result = repository.multiply(a,b);
-    notifyListeners();
+  result = _repository.multiply(a,b);
+  notifyListeners();
   }
 
   void divide (double a, double b){
-    result = repository.divide(a,b);
-    notifyListeners();
+  result = _repository.divide(a,b);
+  notifyListeners();
+    }
   }
-
   /// Removes all items from the cart.
   void removeAll() {
 
     // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
+
   }
-}
+
