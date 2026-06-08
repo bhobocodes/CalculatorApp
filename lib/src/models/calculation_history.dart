6c6c0664afb45e34f7f1208267a144466c9978a6
bhobo <bhobotosh345@gmail.com>
2026-06-08 07:28:44 +0600

    change code

---
+++ lib/src/models/calculation_history.dart
@@ -0,0 +1,10 @@
+class CalculatorHistory {
+  final String expression;
+  final String result;
+
+
+  CalculatorHIstory({
+    required this.expression,
+    required this.result,
+  })
+}
--- lib/src/viewmodel/calculator_model.dart
+++ lib/src/viewmodel/calculator_model.dart
@@ -1,9 +1,27 @@
 class CalculatorModel extends ChangeNotifier {
   CalculatorModel(required this._repository);
-  final Repository _repositroy;
+  final Repository repositroy;
+  double result = 0;
+
   void add(double a , double b) {
     _repositroy.add(a,b);
     // This call tells the widgets that are listening to this model to rebuild.
+    notifyListeners();
+  }
+
+  void subtract(double a, double b){
+    result = repository.subtract(a,b);
+    notifyListeners();
+
+  }
+
+  void multiply (double a, double b){
+    result = repository.multiply(a,b);
+    notifyListeners();
+  }
+
+  void divide (double a, double b){
+    result = repository.divide(a,b);
     notifyListeners();
   }
 
